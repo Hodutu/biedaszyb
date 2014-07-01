b8b374b851021089568eaaa8f7cab46befa2ba38
Michal Budzynski <michal@virtualdesign.pl>
2014-07-02 01:25:52 +0200

    [BIEDASZYB] -  bdsb

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 var http = require('http');
 var dwnld = require('./helpers/download');
 
-//http.createServer(function (req, res) {
-  // var args = req.url.slice(1).split('/');
-  //
-  // var title = args[0];
-  // var start = parseInt(args[1], 10);
-  // var howManyEpisodes = parseInt(args[2], 10);
+http.createServer(function (req, res) {
+  var args = req.url.slice(1).split('/');
 
-  //res.writeHead(200, {'Content-Type': 'text/plain'});
+  var title = args[0];
+  var start = parseInt(args[1], 10);
+  var howManyEpisodes = parseInt(args[2], 10);
 
-  dwnld();
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  dwnld(title, start, start+howManyEpisodes, function(data) {
+    res.write(data.toString());
+    res.end();
+  });
 
 
-//}).listen(1337, '127.0.0.1');
+}).listen(1337, '127.0.0.1');
