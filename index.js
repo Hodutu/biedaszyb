ccfd3159b6ffd3d170718c457ebaec77bacf21f5
Michal Budzynski <michal@virtualdesign.pl>
2014-08-11 01:01:00 +0100

    [BIEDASZYB] - :video_camera: lint fixes

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ var filestube = require('filestube-client');
 //var filebit = require('filebit-client');
 
 var biedaszyb = function(title, first, last, mainCB) {
-  var maxEpisodes = last;
+  //var maxEpisodes = last;
   var episode = first;
   if (episode < 10) {
     episode = '0' + episode;
   }
 
-  var changeLinksToFilebitLinks = function(links){
-
-  };
+  // var changeLinksToFilebitLinks = function(links){
+  //
+  // };
 
   var getLinksFromFilestube = function(title){
     console.log('looking for', title);
     filestube.getAll(title, {type:'mkv'}, function(e){
       console.log('links downloaded from Filestube', e);
-      mainCB(e)
+      mainCB(e);
     });
-  }
+  };
 
   getLinksFromFilestube(title + episode);
 };
