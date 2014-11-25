da3a26e98c237d0f85029bb446a3a8d0d7b2d450
Michał Budzyński <michal@virtualdesign.pl>
2014-11-25 01:01:00 +0100

    :anger: proposed API

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+var biedaszyb = (function(){
+
+  return {
+    show: show,
+    file: file
+  }
+})();
+
+// biedaszyb.show(title, season, firstEpisode, lastEpisode, callback);
+// biedaszyb.file(title, callback);
