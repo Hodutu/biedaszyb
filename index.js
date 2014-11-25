460516f3ef514c4eac9174c6bdb7db9d27bc2c6d
Michał Budzyński <michal@virtualdesign.pl>
2014-11-25 01:01:00 +0100

    :unamused: lint fixes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-
+'use strict';
 
 var biedaszyb = (function(){
+
+  var show = function(){};
+  var file = function(){};
 
   return {
     show: show,
     file: file
-  }
+  };
 })();
+
+module.exports = biedaszyb;
 
 // biedaszyb.show(title, season, firstEpisode, lastEpisode, callback);
 // biedaszyb.file(title, callback);
