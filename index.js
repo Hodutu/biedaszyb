eb36a5d2a8059feba26fb8dfe70cada526c70e9a
Michał Budzyński <michal@virtualdesign.pl>
2014-11-26 01:01:00 +0100

    :turtle: working copy hellyeah!

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var fc = require('filestube-client');
 var fbc = require('filebit-client');
 var biedaconfig = require('./biedaconfig.json');
+var eachSeries = require('async').eachSeries;
 
 var filebitRequest = function(link, cb) {
   fbc.login(
@@ -24,7 +25,6 @@ var filebitRequest = function(link, cb) {
         }
 
         // We have nice link to download!
-        console.log(finalLink);
         cb(null, finalLink);
       });
     }
@@ -37,18 +37,17 @@ var biedaszyb = (function(){
   var file = function(title, cb) {
     console.log('szukam...');
     fc.getAll(title, {}, function(links) {
-      links.forEach(function(link) {
+      eachSeries(links, function(link, esCb) {
         // XXX: multiple links support!
         // This should work also for Array of links
-        filebitRequest(link[0], function(err, result){
-        //filebitRequest('http://d01.megashares.com/dl/Eoq4b6K/How.I.Met.Your
-        //.Mother.S01E01.DVDRip.XviD-TOPAZ.avi', function(err, result){
-          if (err){
+        filebitRequest(link[0], function(err, result) {
+          if (err) {
+            esCb();
             console.log(err);
             return;
           }
 
-          console.log('link finished: ', result);
+          cb(result);
         });
       });
     });
@@ -63,4 +62,6 @@ var biedaszyb = (function(){
 module.exports = biedaszyb;
 
 // biedaszyb.show(title, season, firstEpisode, lastEpisode, callback);
-biedaszyb.file('How I Met Your Mother S01E01');//, callback);
+biedaszyb.file('How I Met Your Mother S01E01', function(result) {
+  console.log('NO HEJKA!', result);
+});//, callback);
