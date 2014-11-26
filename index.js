b516b7dcc7b6556841a0c6af6195897ea335097b
Michał Budzyński <michal@virtualdesign.pl>
2014-11-26 01:01:00 +0100

    :-1: use fbc to convert file

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 'use strict';
 
 var fc = require('filestube-client');
+var fbc = require('filebit-client');
+
+var logged = false;
+var filebitRequest = function(link, cb) {
+  fbc.login('my-login', 'my-password', function(error, loggedIn){
+    if (error) {
+      // we could not logged in
+      cb(error);
+      return;
+    }
+
+    // We are logged in, so let's download some file!
+    fbc.getLink(link, function(error, finalLink) {
+      if (error) {
+        // we could not get proper link
+        cb(error);
+        return;
+      }
+
+      // We have nice link to download!
+      cb(null, finalLink);
+    });
+  });
+};
 
 var biedaszyb = (function(){
 
@@ -8,7 +32,16 @@ var biedaszyb = (function(){
   var file = function(title, cb) {
     console.log('szukam');
     fc.getAll(title, {}, function(links) {
-      console.log(links);
+      links.forEach(function(link){
+        filebitRequest(link, function(err, result){
+          if (err){
+            console.log('cannot get anything smart for ', link);
+            return;
+          }
+
+          console.log('link finished: ', result);
+        });
+      });
     });
   };
 
