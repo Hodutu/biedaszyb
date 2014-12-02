43e206fdc215620941fda97c522402a657eccb4f
Michał Budzyński <michal@virtualdesign.pl>
2014-12-02 06:33:43 +0100

    :european_castle: working server

--- bin/server.js
+++ bin/server.js
@@ -21,8 +21,8 @@ res.writeHead(200, {'Content-Type': 'text/plain'});
 
     console.log(result);
 
-    res.write(result);
-    res.close();
+    res.write(result.toString());
+    res.end();
   });
 
 }).listen(1337, '127.0.0.1');
