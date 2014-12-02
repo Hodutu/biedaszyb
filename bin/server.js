9b986218de52015083d9f256d502008bca153646
Michał Budzyński <michal@virtualdesign.pl>
2014-12-02 06:30:38 +0100

    :mag_right: first attempt to create a server

---
+++ bin/server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var biedaszyb = require('../');
+
+http.createServer(function (req, res) {
+var args = req.url.slice(1).split('/');
+
+var title = args[0];
+var season = parseInt(args[1], 10);
+var start = parseInt(args[2], 10);
+var howManyEpisodes = parseInt(args[3], 10);
+
+res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log('searching...');
+  biedaszyb.show(title, season, start, howManyEpisodes, function(err, result){
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    console.log(result);
+
+    res.write(result);
+    res.close();
+  });
+
+}).listen(1337, '127.0.0.1');
