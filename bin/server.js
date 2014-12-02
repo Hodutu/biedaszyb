7c01a718e1ec085bd043cd2e251e7d3a39d0b13e
Michał Budzyński <michal@virtualdesign.pl>
2014-12-02 06:37:50 +0100

    :closed_book: proper server response

--- bin/server.js
+++ bin/server.js
@@ -4,25 +4,29 @@ var http = require('http');
 var biedaszyb = require('../');
 
 http.createServer(function (req, res) {
-var args = req.url.slice(1).split('/');
+  var args = req.url.slice(1).split('/');
 
-var title = args[0];
-var season = parseInt(args[1], 10);
-var start = parseInt(args[2], 10);
-var howManyEpisodes = parseInt(args[3], 10);
+  var title = args[0];
+  var season = parseInt(args[1], 10);
+  var start = parseInt(args[2], 10);
+  var howManyEpisodes = parseInt(args[3], 10);
 
-res.writeHead(200, {'Content-Type': 'text/plain'});
-  console.log('searching...');
-  biedaszyb.show(title, season, start, howManyEpisodes, function(err, result){
-    if (err) {
-      console.log(err);
-      return;
-    }
+  if (title && season && start && howManyEpisodes) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    console.log('searching...');
+    biedaszyb.show(title, season, start, howManyEpisodes, function(err, result){
+      if (err) {
+        console.log(err);
+        return;
+      }
 
-    console.log(result);
+      console.log(result);
 
-    res.write(result.toString());
-    res.end();
-  });
+      result.forEach(function(link){
+        res.write(link + '\n');
+      });
 
+      res.end();
+    });
+  }
 }).listen(1337, '127.0.0.1');
